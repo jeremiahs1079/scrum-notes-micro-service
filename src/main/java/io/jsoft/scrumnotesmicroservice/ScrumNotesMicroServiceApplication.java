65eaa4ab566123adbde809356ae81d06333b3ab7
Jeremiah Smith <jeremiahs1079@hotmail.com>
2019-02-12 14:29:00 -0500

    initial commit

---
+++ src/main/java/io/jsoft/scrumnotesmicroservice/ScrumNotesMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package io.jsoft.scrumnotesmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumNotesMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumNotesMicroServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/jsoft/scrumnotesmicroservice/ScrumNotesMicroServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package io.jsoft.scrumnotesmicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScrumNotesMicroServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
